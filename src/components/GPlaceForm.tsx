4c4ffd5e71397e8537d5cef09d4a86feea6c117e
Michael Padin <padinmichael201@gmail.com>
2023-08-28 19:06:18 +0800

    feat: add google place auto complete component

---
+++ src/components/GPlaceForm.tsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { env } from "~/env.mjs";
+import { useForm } from "react-hook-form";
+import usePlacesService from "react-google-autocomplete/lib/usePlacesAutocompleteService";
+
+import { Button } from "./ui/button";
+import { Form, FormField } from "./ui/form";
+import { useToast } from "./ui/use-toast";
+
+import { GPlaceInput } from "./GPlaceInput";
+
+const FormSchema = z.object({
+  place: z
+    .string({
+      required_error: "Please enter a place.",
+    })
+    .min(5, { message: "Must be 5 or more characters long" }),
+});
+
+export default function GPlaceForm() {
+  const { toast } = useToast();
+  const [placeListOPen, setPlaceListOPen] = useState(false);
+  const { placePredictions, getPlacePredictions } = usePlacesService({
+    apiKey: env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    debounce: 500,
+  });
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  const onSubmit = (data: z.infer<typeof FormSchema>) => {
+    console.log(data);
+
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-xs text-white">{JSON.stringify(data)}</code>
+        </pre>
+      ),
+    });
+  };
+  const handlePlaceValue = (place: string) => {
+    // Keep the options displayed when the user is typing
+    if (!placeListOPen) setPlaceListOPen(true);
+
+    form.setValue("place", place);
+    getPlacePredictions({ input: place });
+  };
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className=" space-y-6">
+        <FormField
+          control={form.control}
+          name="place"
+          render={({ field }) => (
+            <GPlaceInput
+              value={field.value}
+              placeListOpen={placeListOPen}
+              handleSelect={(placeName) => {
+                setPlaceListOPen(false);
+                form.setValue("place", placeName);
+              }}
+              placePredictions={placePredictions}
+              handlePlaceValue={handlePlaceValue}
+            />
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
---
+++ src/components/GPlaceInput.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { FormControl, FormItem, FormLabel, FormMessage } from "./ui/form";
+import { Command, CommandGroup, CommandItem, CommandList } from "./ui/command";
+import { Input } from "./ui/input";
+import type { PlaceData, PlaceFormFieldProps } from "types";
+
+/**
+ *
+ * @param value: This parameter represents the current value of the input field for the place. It's used to set the value of the input element.
+ * @param placeListOpen: This parameter is a boolean that indicates whether the list of place predictions is currently open or closed. It controls the visibility of the prediction list.
+ * @param placePredictions: This parameter is an array of predicted place data. It contains suggestions for places based on user input.
+ * @param handleSelect: This is a callback function that gets executed when a user selects a place from the list of predictions. It typically updates the selected place.
+ * @param handlePlaceValue: This is a callback function that gets executed when the value of the place input field changes. It is responsible for updating the input field value and fetching new predictions based on user input.
+ * @returns
+ */
+
+export function GPlaceInput({
+  value,
+  placeListOpen,
+  placePredictions,
+  handleSelect,
+  handlePlaceValue,
+}: PlaceFormFieldProps) {
+  return (
+    <FormItem className="flex flex-col">
+      <FormLabel className="text-base">Place</FormLabel>
+      <Command className="relative overflow-visible">
+        <FormControl className="text-base">
+          <Input
+            placeholder="Search place..."
+            className="w-full"
+            onChange={(e) => handlePlaceValue(e.target.value)}
+            value={value}
+            type="text"
+          />
+        </FormControl>
+        {placeListOpen && (
+          <CommandList
+            className={`absolute left-0 right-0 top-[46px] rounded-lg bg-white ${
+              placePredictions.length > 0 ? " ring-1 ring-slate-200 animate-in fade-in-0 zoom-in-95" : ""
+            }`}
+          >
+            <CommandGroup
+              className={`${placePredictions.length > 0 ? "p-2" : "p-0"} `}
+            >
+              {placePredictions.map((place: PlaceData) => (
+                <CommandItem
+                  key={place.place_id}
+                  onSelect={() => handleSelect(place?.description)}
+                  className="text-base"
+                >
+                  {place.description}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        )}
+      </Command>
+      <FormMessage />
+    </FormItem>
+  );
+}
